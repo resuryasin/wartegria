557af456de5ce83fcfe402cdcbdc9ed80a349f6a
resuryasin <surya.d.pradana@gmail.com>
2023-10-01 20:29:13 +0700

    Add api for app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_restful import Api
 
 app = Flask(__name__) 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///warteg.db'
 db = SQLAlchemy(app)
+api = Api(app)
 CORS(app)
