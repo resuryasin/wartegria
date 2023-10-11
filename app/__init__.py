c859eb349cd31179cb57d8020880cd2138d11f48
resuryasin <surya.d.pradana@gmail.com>
2023-10-12 05:36:15 +0700

    Update init

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,13 @@ from app.config import SECRET_KEY, SQLALCHEMY_DATABASE_URI
 app = Flask(__name__) 
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.static_folder = 'public'
 
 db = SQLAlchemy(app)
 api = Api(app)
 CORS(app)
 
-from app.resources import WartegResorce
-api.add_resource(WartegResorce, '/api/warteg/')
+from app.resources import WartegResource
+api.add_resource(WartegResource, '/api/warteg/')
 
 from app.views import home
